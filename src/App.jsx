6f76bef4b21f8a0e25c13bb981338f31f68f4419
Rahul <rahulrawattrippy@gmail.com>
2023-07-10 14:40:21 +0530

    complete for web

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
-import React from 'react'
+import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import "./App.css";
 
 function App() {
   const { unityProvider } = useUnityContext({
@@ -10,11 +10,16 @@ function App() {
     codeUrl: "buildUnity/Game1.wasm.unityweb",
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  return (
+    <div className="app">
+      <div className="container">
+        <Unity unityProvider={unityProvider} />
+      </div>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
 
 //dev command : npm run dev
-//test with the smaller "flagbuild" files. 
+//test with the smaller "flagbuild" files.
